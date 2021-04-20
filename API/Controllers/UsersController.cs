94e1e1183cd626e684004939c6fc2e2e87691224
Phantompack <2016106037@ufs4life.ac.za>
2021-04-20 03:33:49 +0200

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using API.Interfaces;
 using API.DTOs;
 using AutoMapper;
-
+using  System.Security.Claims;
 namespace API.Controllers
 {
     [Authorize]
@@ -38,6 +38,23 @@ namespace API.Controllers
             
                
         }
+
+// api/users
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+
+        _mapper.Map(memberUpdateDto, user);
+
+        _userRepository.Update(user);
+
+        if (await _userRepository.SaveAllAsync()) return NoContent();
+        return BadRequest("Failed to update user");
+     }
+
+
 
     }
 }
