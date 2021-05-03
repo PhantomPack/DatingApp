239bad21be11fe4907f0351e906e688932a0aa83
Phantompack <2016106037@ufs4life.ac.za>
2021-05-03 10:49:06 +0200

    End of section 14

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -56,7 +56,7 @@ export class MembersService {
     params  = params.append('orderBy', userParams.orderBy);
     
 
-    return this.getPaginatedResult<Member[]>(this.baseUrl,params)
+    return this.getPaginatedResult<Member[]>(this.baseUrl+ 'users',params)
     .pipe(map(response => {
       this.memberCache.set(Object.values(userParams).join('-'), response)
       return response;
@@ -92,11 +92,29 @@ export class MembersService {
   deletePhoto(photoId: number) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
+
+
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+  
+  getLikes(predicate: string,  pageNumber: number,  pageSize:number) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('predicate', predicate);
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl+ 'likes', params);
+
+ }
+  
+  
+  
+  
+  
+
   
   private getPaginatedResult<T>(url: string , params: HttpParams) {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
     return this.http
-      .get<T>(url + 'users', { observe: 'response', params })
+      .get<T>(url, { observe: 'response', params })
       .pipe(
         map(response => {      
             paginatedResult.result = response.body as T;
